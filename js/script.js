b4253c0d49041b1c555dacc376b7206e2db7f402
dalv95 <dalv95@gmail.com>
2024-02-18 17:35:10 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function clickUslugi() {
+    document.getElementById('uslugi').scrollIntoView();
+}
+
+function clickKontakt(){
+    document.getElementById('kontakt').scrollIntoView();
+}
+
+function clickProjekty(){
+    document.getElementById('projekty').scrollIntoView();
+}
+
+function myModal() {
+    document.getElementById('modal').style.display = 'block'
+}
+
+function exitModal() {
+    document.getElementById('modal').style.display = 'hidden'
+}
